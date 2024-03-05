a00ca82757e055a93dd591d9d8414a848478dc39
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2024-03-05 23:01:02 +0100

    Fix indentation

--- sensor/customsensor.go
+++ sensor/customsensor.go
@@ -4,101 +4,101 @@
 package customsensor
 
 import (
-	"context"
-	"errors"
+    "context"
+    "errors"
 
-	"go.viam.com/rdk/components/sensor"
-	"go.viam.com/rdk/logging"
-	"go.viam.com/rdk/resource"
+    "go.viam.com/rdk/components/sensor"
+    "go.viam.com/rdk/logging"
+    "go.viam.com/rdk/resource"
 
-	"go.viam.com/utils"
+    "go.viam.com/utils"
 )
 
 // Here is where we define your new model's colon-delimited-triplet (viam-labs:go-module-templates-sensor:customsensor)
 // viam-labs = namespace, go-module-templates-sensor = repo-name, customsensor = model name.
 // TODO: Change model namespace, family (often the repo-name), and model. For more information see https://docs.viam.com/registry/create/#name-your-new-resource-model
 var (
-	Model            = resource.NewModel("viam-labs", "go-module-templates-sensor", "customsensor")
-	errUnimplemented = errors.New("unimplemented")
+    Model            = resource.NewModel("viam-labs", "go-module-templates-sensor", "customsensor")
+    errUnimplemented = errors.New("unimplemented")
 )
 
 func init() {
-	resource.RegisterComponent(sensor.API, Model,
-		resource.Registration[sensor.Sensor, *Config]{
-			Constructor: newCustomSensor,
-		},
-	)
+    resource.RegisterComponent(sensor.API, Model,
+        resource.Registration[sensor.Sensor, *Config]{
+            Constructor: newCustomSensor,
+        },
+    )
 }
 
 // TODO: Change the Config struct to contain any values that you would like to be able to configure from the attributes field in the component
 // configuration. For more information see https://docs.viam.com/build/configure/#components
 type Config struct {
-	ArgumentOne int    `json:"one"`
-	ArgumentTwo string `json:"two"`
+    ArgumentOne int    `json:"one"`
+    ArgumentTwo string `json:"two"`
 }
 
 // Validate validates the config and returns implicit dependencies.
 // TODO: Change the Validate function to validate any config variables.
 func (cfg *Config) Validate(path string) ([]string, error) {
-	if cfg.ArgumentOne == 0 {
-		return nil, utils.NewConfigValidationFieldRequiredError(path, "one")
-	}
+    if cfg.ArgumentOne == 0 {
+        return nil, utils.NewConfigValidationFieldRequiredError(path, "one")
+    }
 
-	if cfg.ArgumentTwo == "" {
-		return nil, utils.NewConfigValidationFieldRequiredError(path, "two")
-	}
+    if cfg.ArgumentTwo == "" {
+        return nil, utils.NewConfigValidationFieldRequiredError(path, "two")
+    }
 
-	// TODO: return implicit dependencies if needed as the first value
-	return []string{}, nil
+    // TODO: return implicit dependencies if needed as the first value
+    return []string{}, nil
 }
 
 // Constructor for a custom sensor that creates and returns a customSensor.
 // TODO: update the customSensor struct and the initialization.
 func newCustomSensor(ctx context.Context, deps resource.Dependencies, rawConf resource.Config, logger logging.Logger) (sensor.Sensor, error) {
-	// This takes the generic resource.Config passed down from the parent and converts it to the
-	// model-specific (aka "native") Config structure defined above, making it easier to directly access attributes.
-	conf, err := resource.NativeConfig[*Config](rawConf)
-	if err != nil {
-		return nil, err
-	}
+    // This takes the generic resource.Config passed down from the parent and converts it to the
+    // model-specific (aka "native") Config structure defined above, making it easier to directly access attributes.
+    conf, err := resource.NativeConfig[*Config](rawConf)
+    if err != nil {
+        return nil, err
+    }
 
-	// Create a cancelable context for custom sensor
-	cancelCtx, cancelFunc := context.WithCancel(context.Background())
+    // Create a cancelable context for custom sensor
+    cancelCtx, cancelFunc := context.WithCancel(context.Background())
 
-	s := &customSensor{
-		name:        rawConf.ResourceName(),
-		logger:      logger,
-		cfg:         conf,
-		cancelCtx:   cancelCtx,
-		cancelFunc:  cancelFunc,
-	}
+    s := &customSensor{
+        name:        rawConf.ResourceName(),
+        logger:      logger,
+        cfg:         conf,
+        cancelCtx:   cancelCtx,
+        cancelFunc:  cancelFunc,
+    }
 
-	// TODO: If your custom component has dependencies, perform any checks you need to on them.
+    // TODO: If your custom component has dependencies, perform any checks you need to on them.
 
-	// The Reconfigure() method changes the calues on the customSensor based on the attributes in the component config
-	if err := s.Reconfigure(ctx, deps, rawConf); err != nil {
-		logger.Error("Error configuring module with ", err)
-		return nil, err
-	}
+    // The Reconfigure() method changes the calues on the customSensor based on the attributes in the component config
+    if err := s.Reconfigure(ctx, deps, rawConf); err != nil {
+        logger.Error("Error configuring module with ", rawConf)
+        return nil, err
+    }
 
-	return s, nil
+    return s, nil
 }
 
 // TODO: update the customSensor struct with any fields you require.
 type customSensor struct {
-	name   resource.Name
-	logger logging.Logger
-	cfg    *Config
+    name   resource.Name
+    logger logging.Logger
+    cfg    *Config
 
-	cancelCtx  context.Context
-	cancelFunc func()
+    cancelCtx  context.Context
+    cancelFunc func()
 
-	argumentOne int
-	argumentTwo string
+    argumentOne int
+    argumentTwo string
 }
 
 func (s *customSensor) Name() resource.Name {
-	return s.name
+    return s.name
 }
 
 // Reconfigures the model. Most models can be reconfigured in place without needing to rebuild. If you need to instead create a new instance of the sensor, throw a NewMustBuildError.
@@ -111,7 +111,7 @@ func (s *customSensor) Reconfigure(ctx context.Context, deps resource.Dependenci
 
     s.argumentOne = sensorConfig.ArgumentOne
     s.argumentTwo = sensorConfig.ArgumentTwo
-	s.name = conf.ResourceName()
+    s.name = conf.ResourceName()
     s.logger.Info("one is now configured to: ", s.argumentOne)
     s.logger.Info("two is now configured to ", s.argumentTwo)
 
@@ -119,19 +119,19 @@ func (s *customSensor) Reconfigure(ctx context.Context, deps resource.Dependenci
 }
 
 func (s *customSensor) Readings(context.Context, map[string]interface{}) (map[string]interface{}, error) {
-	// TODO: Obtain and return readings.
-	s.logger.Error("Method unimplemented")
-	return nil, errUnimplemented
+    // TODO: Obtain and return readings.
+    s.logger.Error("Method unimplemented")
+    return nil, errUnimplemented
 }
 
 // DoCommand is a place to add additional commands to extend the sensor API. This is optional.
 func (s *customSensor) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
-	s.logger.Error("Method unimplemented")
-	return nil, errUnimplemented
+    s.logger.Error("Method unimplemented")
+    return nil, errUnimplemented
 }
 
 // Close closes the underlying generic.
 func (s *customSensor) Close(ctx context.Context) error {
-	s.cancelFunc()
-	return nil
+    s.cancelFunc()
+    return nil
 }
